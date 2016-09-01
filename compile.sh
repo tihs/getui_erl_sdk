9a2064beeb5e13151ac9db491d2d3feca7fdd5c5
zuti <zuti@avatarworks.com>
2016-09-01 10:15:53 +0800

    xx

--- compile.sh
+++ compile.sh
@@ -8,6 +8,7 @@ cd $real_path
 mkdir -p priv
 gcc -fPIC -O3 -c c_src/equeue.c -o c_src/equeue.o -I /usr/local/lib/erlang/usr/include
 g++ -shared -fPIC -O3 -o /usr/local/lib/libgtpushsdk_mid.so c_src/gtpushsdk_mid.cpp  -lgtpushsdk_gcc4.1.2_64 -L lib/ -I c_src/
+ldconfig
 #cp priv/libgtpushsdk_mid.so /usr/local/lib
 gcc --shared -fPIC -O3 c_src/equeue.o -o priv/getui_nif_async.so c_src/getui_nif_async.c  -I . -I /usr/local/lib/erlang/usr/include -I c_src/  -L lib/ -lpthread -ldl -lrt -lgtpushsdk_mid
 #gcc --shared -fPIC c_src/equeue.o -o priv/getui_nif_async.so c_src/getui_nif_async.c  -I . -I /usr/local/lib/erlang/usr/include -I c_src/  -L priv/ -lpthread -ldl -lrt -lgtpushsdk_mid  -D DEBUG
