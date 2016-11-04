e9c9594537a05c01251115609338f499f2d9a6ea
tidaybreak <tidaybreak@gmail.com>
2016-11-04 12:00:06 +0800

    xxxxxxx

--- c_src/getui_nif_async.d
+++ c_src/getui_nif_async.d
@@ -1,7 +1,6 @@
 c_src/getui_nif_async.o: c_src/getui_nif_async.c \
- /usr/local/lib/erlang/erts-7.0/include/erl_nif.h \
- /usr/local/lib/erlang/erts-7.0/include/erl_native_features_config.h \
- /usr/local/lib/erlang/erts-7.0/include/erl_drv_nif.h \
- /usr/local/lib/erlang/erts-7.0/include/erl_int_sizes_config.h \
- /usr/local/lib/erlang/erts-7.0/include/erl_nif_api_funcs.h \
+ /usr/local/lib/erlang/erts-8.1/include/erl_nif.h \
+ /usr/local/lib/erlang/erts-8.1/include/erl_drv_nif.h \
+ /usr/local/lib/erlang/erts-8.1/include/erl_int_sizes_config.h \
+ /usr/local/lib/erlang/erts-8.1/include/erl_nif_api_funcs.h \
  c_src/IGtPush.h c_src/equeue.h
